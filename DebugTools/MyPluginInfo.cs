9ff418d477925e1b97edd3a068aa5f23e3e8832a
Nagord <46509577+Nagord@users.noreply.github.com>
2026-03-09 13:24:02 -0700

    Add project files.

---
+++ DebugTools/MyPluginInfo.cs
@@ -0,0 +1,15 @@
+namespace DebugTools
+{
+    //Auto-Generated File. Created by PreBuild.ps1
+    public class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "";
+        public const string PLUGIN_NAME = "DebugTools";
+        public const string USERS_PLUGIN_NAME = "DebugTools";
+        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_DESCRIPTION = "";
+        public const string PLUGIN_ORIGINAL_AUTHOR = "";
+        public const string PLUGIN_AUTHORS = "";
+        public const string PLUGIN_THUNDERSTORE_ID = "";
+    }
+}
---
+++ DebugTools/Plugins.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using VoidManager;
+using VoidManager.MPModChecks;
+
+namespace DebugTools
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.USERS_PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Void Crew.exe")]
+    [BepInDependency(VoidManager.MyPluginInfo.PLUGIN_GUID)]
+    public class BepinPlugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "N/A")]
+        private void Awake()
+        {
+            Log = Logger;
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+
+
+    public class VoidManagerPlugin : VoidPlugin
+    {
+        public VoidManagerPlugin()
+        {
+
+        }
+
+        public override MultiplayerType MPType => MultiplayerType.Host;
+
+        public override string Author => MyPluginInfo.PLUGIN_AUTHORS;
+
+        public override string Description => MyPluginInfo.PLUGIN_DESCRIPTION;
+
+        public override string ThunderstoreID => MyPluginInfo.PLUGIN_THUNDERSTORE_ID;
+
+        public override SessionChangedReturn OnSessionChange(SessionChangedInput input)
+        {
+            if (input.IsHost || input.HostHasMod)
+            {
+
+            }
+
+            return base.OnSessionChange(input);
+        }
+    }
+}
